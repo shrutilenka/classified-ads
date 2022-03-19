d861bfbeb7f09540e931433a9c41697ba6f2392e
bacloud14 <bacloud14@gmail.com>
2022-03-19 08:38:19 +0100

    fix promisify

--- libs/decorators/postSectionHandler.js
+++ libs/decorators/postSectionHandler.js
@@ -1,7 +1,8 @@
 const { Storage } = require('@google-cloud/storage')
 // const Joi = require('joi')
 const path = require('path')
-const { format } = require('util')
+const { format, promisify } = require('util')
+
 const storage = new Storage({ keyFilename: process.env.CREDS_PATH })
 const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET)
 const { validationPipeLine, stringTransformer } = require('../services/PipeLine.js')
@@ -9,6 +10,8 @@ const queries = require('../services/mongo')
 // Require dependencies (fastify plugins and others)
 require('dotenv').config()
 const config = require('config')
+var tidy = require('htmltidy2').tidy
+const tidyP = promisify(tidy)
 // incremental is better at least here in app.js
 const NODE_ENV = {
     'monkey chaos': -1,
@@ -44,7 +47,7 @@ const insertDocument = async (QInstance, req, blob, upload) => {
     let acknowledged = await QInstance.insertDocument(entry)
     return { data: entry, messages: [] }    
 }
-var tidy = require('htmltidy2').tidy;
+
 
 // options http://api.html-tidy.org/tidy/tidylib_api_5.6.0/tidy_quickref.html
 const opt = { 'show-body-only': 'yes' }
@@ -66,37 +69,34 @@ module.exports = (fastify) => {
                 error: error
             })
         } else {
-            tidy(body.desc, opt, async function (err, html) {
-                body.desc = new stringTransformer(html).sanitizeHTML().cleanSensitive().valueOf()
-                // Files other than images are undefined
-                if (!req.file && NODE_ENV > 0) {
-                    reply.send({
-                        message: 'Invalid request',
-                        data: body,
-                        error: 'file not found'
-                    })
-                }
-                if (NODE_ENV < 1) {
-                    let data = await insertDocument(QInstance, req, null, false)
+            const html = await tidyP(body.desc, opt)
+            body.desc = new stringTransformer(html).sanitizeHTML().cleanSensitive().valueOf()
+            // Files other than images are undefined
+            if (!req.file && NODE_ENV > 0) {
+                reply.send({
+                    message: 'Invalid request',
+                    data: body,
+                    error: 'file not found'
+                })
+            }
+            if (NODE_ENV < 1) {
+                let data = await insertDocument(QInstance, req, null, false)
+                reply.blabla([data, 'listing', section])
+            } else {
+                // Upload that damn picture
+                // Create a new blob in the bucket and upload the file data.
+                const suffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+                const filename = suffix + path.extname(req.file.originalname)
+                const blob = bucket.file(filename)
+                const blobStream = blob.createWriteStream()
+                blobStream.on('error', (err) => { throw (err) })
+                blobStream.on('finish', async () => {
+                    let data = await insertDocument(QInstance, req, blob, true)
                     reply.blabla([data, 'listing', section])
-                } else {
-                    // Upload that damn picture
-                    // Create a new blob in the bucket and upload the file data.
-                    const suffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-                    const filename = suffix + path.extname(req.file.originalname)
-                    const blob = bucket.file(filename)
-                    const blobStream = blob.createWriteStream()
-                    blobStream.on('error', (err) => {
-                        // TODO: do not call done inside promises
-                        throw (err)
-                    })
-                    blobStream.on('finish', async () => {
-                        let data = await insertDocument(QInstance, req, blob, true)
-                        reply.blabla([data, 'listing', section])
-                    })
-                    blobStream.end(req.file.buffer)
-                }
-            })
+                })
+                blobStream.end(req.file.buffer)
+            }
+            
         }
     }
 }
