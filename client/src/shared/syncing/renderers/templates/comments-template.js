29c7a27d77a95302b8cbc7da4154ce33ad85d8ab
bacloud14.ubu <bacloud14@gmail.com>
2022-04-08 08:57:42 +0200

    tweak style

--- client/src/shared/syncing/renderers/templates/comments-template.js
+++ client/src/shared/syncing/renderers/templates/comments-template.js
@@ -8,23 +8,23 @@ export const commentsTemplate = `
 
 
             <% if (comments[i].from === user.nickname) { %>
-                <div class="chat chat-darker" x-on:click="open = ! open">
+                <div class="comment comment-darker" x-on:click="open = ! open">
                     <img src="/images/1546457182.svg" alt="<%= comments[i].from %>" class="right">
                     <p><%= comments[i].message %></p>
-                    <span class="chat-time-left"><%= new Date(parseInt(comments[i]._id.toString().substring(0, 8), 16) * 1000).toISOString().slice(0,10) %></span>
+                    <span class="comment-time-left"><%= new Date(parseInt(comments[i]._id.toString().substring(0, 8), 16) * 1000).toISOString().slice(0,10) %></span>
                 </div>
             <% } else { %>
-                <div class="chat" x-on:click="open = ! open">
+                <div class="comment" x-on:click="open = ! open">
                 <img  alt="Red dot" />
                     <img src="/images/1546457622.svg" alt="<%= comments[i].from %>">
                     <p><%= comments[i].message %></p>
-                    <span class="chat-time-right"><%= new Date(parseInt(comments[i]._id.toString().substring(0, 8), 16) * 1000).toISOString().slice(0,10) %></span>
+                    <span class="comment-time-right"><%= new Date(parseInt(comments[i]._id.toString().substring(0, 8), 16) * 1000).toISOString().slice(0,10) %></span>
                 </div>
             <% }%>
 
             <% if (typeof user != 'undefined') { %>
                 <span x-show="open" x-transition>
-                    <div class="chat chat-reply">
+                    <div class="comment comment-reply">
                         <div>
                             <button class="btn btn-primary my-2 w-25" value="replyTo" onclick="postComment('<%= comments[i]._id %>')">Reply to this ...</button>
                         </div>
