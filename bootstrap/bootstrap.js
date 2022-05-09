f1beef94ba773779666a2509511e6804ba45c50b
bacloud14.ubu <bacloud14@gmail.com>
2022-05-09 15:52:07 +0200

    updating dependencies

--- bootstrap/bootstrap.js
+++ bootstrap/bootstrap.js
@@ -61,7 +61,7 @@ function getRandomInRange(from, to, fixed) {
     return (Math.random() * (to - from) + from).toFixed(fixed) * 1
 }
 
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < 1000; i++) {
     let email
     if (i < 10) {
         email = 'bacloud14@gmail.com'
