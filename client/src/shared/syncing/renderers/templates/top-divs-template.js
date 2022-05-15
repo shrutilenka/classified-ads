92e6d2c7d0368f3de85d29830ab8e21f03d02217
bacloud14 <bacloud14@gmail.com>
2022-05-15 14:59:43 +0200

    fix error on dev env

--- client/src/shared/syncing/renderers/templates/top-divs-template.js
+++ client/src/shared/syncing/renderers/templates/top-divs-template.js
@@ -1,7 +1,7 @@
 export const topDivsTemplate = `
 <nav class="nav d-flex justify-content-between">
     <% for (var i = 0; tags.length> 0 && i < tags.length && i < 15 ; i++) { %>
-        <a href="/tag/<%=tags[i].tag %>" class="p-2 link-secondary"><%=tags[i].tag %></a>
+        <a href="/division/<%=tags[i].tag %>" class="p-2 link-secondary"><%=tags[i].tag %></a>
     <% }%>
 </nav>
 `
