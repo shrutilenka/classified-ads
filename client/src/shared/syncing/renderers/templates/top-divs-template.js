0beeff5dad2a31fe6b559e8218ec0339afbbb25d
bacloud14.ubu <bacloud14@gmail.com>
2022-05-15 08:27:56 +0200

    wip layout, finished tags

--- client/src/shared/syncing/renderers/templates/top-divs-template.js
+++ client/src/shared/syncing/renderers/templates/top-divs-template.js
@@ -1,5 +1,7 @@
 export const topDivsTemplate = `
-    <% for (var i = 0; tags.length> 0 && i < tags.length && i < 10 ; i++) { %>
-        <a href="/tag/<%=tags[i].tag %>" class="badge badge-info"><%=tags[i].tag %></a>
-    <% }%>    
+<nav class="nav d-flex justify-content-between">
+    <% for (var i = 0; tags.length> 0 && i < tags.length && i < 15 ; i++) { %>
+        <a href="/tag/<%=tags[i].tag %>" class="p-2 link-secondary"><%=tags[i].tag %></a>
+    <% }%>
+</nav>
 `
