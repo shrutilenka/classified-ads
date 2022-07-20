dfc7d25fcc2b5e2f3323a4e04e0bc50b0a66b3cc
bacloud14.ubu <bacloud14@gmail.com>
2022-07-20 17:39:49 +0200

    style respond

--- client/src/shared/ads/helpers/adsPolicy.js
+++ client/src/shared/ads/helpers/adsPolicy.js
@@ -4,7 +4,7 @@ import { noAds } from "../state.js";
 export function checkPolicy(){
     const inapp = new InApp(navigator.userAgent || navigator.vendor || window.opera);
     const __grade__ = window.__grade__
-    const __urls__ = window.__urls__
+    const __urls__ = window.__urls__ || []
     noAds.choice = ['development', 'localhost'].includes(process.env.NODE_ENV) || __grade__ <= 1 || inapp.isMobile
     noAds.grade = __grade__
     if(!noAds.choice) {
