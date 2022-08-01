9dc340e4d2b0d2a8a848288b4b1c169c0babca69
bacloud14.ubu <bacloud14@gmail.com>
2022-08-01 15:59:53 +0200

    propose sell

--- client/src/shared/maps/setup-maps.js
+++ client/src/shared/maps/setup-maps.js
@@ -67,7 +67,7 @@ export const setupMaps = () => {
         const markers = new MarkerClusterGroup()
         for (let i = 0; i < addressPoints.length; i++) {
             const a = addressPoints[i]
-            const title = `<a href='${consts.APIHost[process.env.NODE_ENV]}/listings/id/${a[3]}'>${a[2]}</a>`
+            const title = `<a href='${consts.APIHost[process.env.NODE_ENV]}/listings/id/${a[3]}'>${a[2]}/</a>`
             let marker
             if (a[4]) {
                 const icon = getIcon(colors[a[4]])
